5d6c50c268af898633e271eeab1f01a3e22d83fd
Just-Kundai <kundaikhuleya@outlook.com>
2021-11-14 20:45:50 +1100

    updated changes

---
+++ src/components/homepage/HeaderA.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class HeaderA extends Component {
+  render() {
+    return (
+      <div class=:"headerA">
+       
+
+      </div>
+    )
+  }
+}
+
+
+export default HeaderA;
