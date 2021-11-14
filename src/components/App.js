669ce7c5a40a2b657db7568c0309873a315b241a
Natalia Baskakova <natalia.bask@Natalias-MacBook-Air.local>
2021-11-15 09:35:40 +1000

    first copy

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+// import './App.css';
+import Home from './Home'
+import Header from './Header'
+import Banner from './Banner'
+
+function App() {
+  return (
+
+    <div className="app">
+
+<Header />
+
+<Home />
+
+
+
+
+
+    </div>
+
+  );
+}
+export default App
