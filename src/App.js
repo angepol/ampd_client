bbb6b8853999eb7a5fbebfcb6c641ca1b634e8d9
Just-Kundai <kundaikhuleya@outlook.com>
2021-11-15 22:45:55 +1100

    last commit of the day

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 // import './App.css';
 import React from 'react';
-<<<<<<< HEAD
 import Footer from './components/Footer';
 import FooterStyle from './components/FooterStyle';
 import FindSpace from './components/pages/FindSpace';
-=======
 import MainSiteA from './components/MainSiteA'
 import Home from './components/pages/Home'
 import Header from './components/Header'
 import Banner from './components/Banner'
 
 
->>>>>>> 68ef2ae124d1ae455fdad27899eaee0444dc50f4
+
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
 
     <div >
+      <FindSpace />
       <Footer />
     </div>
 
 
-
-
-
   );
 }
-<<<<<<< HEAD
+
 export default App;
-=======
-export default App
->>>>>>> 68ef2ae124d1ae455fdad27899eaee0444dc50f4
