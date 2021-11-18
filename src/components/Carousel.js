3ca4b651b853142ff4ddecc763349ac6f9e9ee75
Natalia Baskakova <natalia.bask@Natalias-MacBook-Air.local>
2021-11-18 11:50:53 +1000

    changes thu

---
+++ src/components/Carousel.js
@@ -0,0 +1,25 @@
+// import React from 'react'
+// import "./Carousel.css"
+
+// const Carousel = props => {
+  //  let ref =
+//     useEffect(() => {
+//
+//
+//     }
+//   )
+//
+//   return (
+//
+//     ref={ref}
+//     className={prop._class}
+//     >
+//     {
+//       React.Children.map(props.children, child => React.Children.only(child))
+//     }
+//     </div>
+//
+//   )
+//
+// }
+// export default Carousel;
